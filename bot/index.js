8c7f5c4282dd20f6f0e33d5025d8dd99573752b1
brendan.mcmahon.bloomerang <brendanjeffreymcmahon@gmail.com>
2025-01-05 20:07:39 -0500

    log updates

--- bot/index.js
+++ bot/index.js
@@ -1,105 +1,20 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const AWS = require('aws-sdk');
+const { saveUser, getGoals, updateGoals } = require('./repository');
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+exports.dynamoDb = dynamoDb;
 const goalsTable = 'GoaliphantGoals';
+exports.goalsTable = goalsTable;
 const userTable = 'GoaliphantUsers';
+exports.userTable = userTable;
 
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token);
 
 const TIME_ZONE = 'America/Indiana/Indianapolis';
-
-function getLocalDate() {
-	const date = new Date();
-	const localDate = date.toLocaleString('en-US', { timeZone: TIME_ZONE });
-	return new Date(localDate).toISOString().split('T')[0];
-}
-
-async function saveGoals(chatId, goals) {
-	const date = getLocalDate();
-
-	const formattedGoals = goals.map(goal => ({ text: goal, completed: false }));
-
-	const params = {
-		TableName: goalsTable,
-		Item: {
-			chatId: chatId.toString(),
-			date: date,
-			goals: formattedGoals,
-		},
-	};
-
-	try {
-		await dynamoDb.put(params).promise();
-		console.log('Goals saved successfully');
-	} catch (err) {
-		console.error('Error saving goals:', err);
-		throw err;
-	}
-}
-
-async function getGoals(chatId) {
-	const date = getLocalDate();
-
-	const params = {
-		TableName: goalsTable,
-		Key: {
-			chatId: chatId.toString(),
-			date: date,
-		},
-	};
-
-	try {
-		const result = await dynamoDb.get(params).promise();
-		return result.Item ? result.Item.goals : [];
-	} catch (err) {
-		console.error('Error fetching goals:', err);
-		throw err;
-	}
-}
-
-
-async function updateGoals(chatId, goals) {
-	const date = new Date().toISOString().split('T')[0];
-	const params = {
-		TableName: goalsTable,
-		Key: {
-			chatId: chatId.toString(),
-			date: date,
-		},
-		UpdateExpression: 'set goals = :goals',
-		ExpressionAttributeValues: {
-			':goals': goals,
-		},
-	};
-
-	try {
-		await dynamoDb.update(params).promise();
-		console.log('Goals updated successfully');
-	} catch (err) {
-		console.error('Error updating goals:', err);
-		throw err;
-	}
-}
-
-async function saveUser(chatId) {
-	const params = {
-		TableName: userTable,
-		Item: {
-			ChatId: chatId.toString(),
-		},
-	};
-
-	try {
-		await dynamoDb.put(params).promise();
-		console.log('User saved successfully');
-	} catch (err) {
-		console.error('Error saving user:', err);
-		throw err;
-	}
-}
+exports.TIME_ZONE = TIME_ZONE;
 
 exports.handler = async (event) => {
 	const body = JSON.parse(event.body);
@@ -180,7 +95,8 @@ async function completeGoal(text, chatId) {
 	try {
 		const goals = await getGoals(chatId);
 		if (index >= 0 && index < goals.length) {
-			goals[index].completed = true;
+			console.log('Completing goal:', goals[index]);
+			goals[index + 1].completed = true;
 			await updateGoals(chatId, goals);
 			await bot.sendMessage(chatId, 'Goal marked as completed.');
 			await listGoals(chatId);
