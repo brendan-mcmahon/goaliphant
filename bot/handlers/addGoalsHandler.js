d5cf15c22e8bac263e3d681bb4c890def6f7cb36
brendan.mcmahon.bloomerang <brendanjeffreymcmahon@gmail.com>
2025-01-12 12:35:04 -0500

    Sending message to wrong chat

--- bot/handlers/addGoalsHandler.js
+++ bot/handlers/addGoalsHandler.js
@@ -20,9 +20,7 @@ async function saveGoalsAndList(newGoals, chatId, fromPartner = false) {
 		const existingGoals = await getGoals(chatId);
 		const updatedGoals = [...existingGoals, ...newGoals.map(goal => ({ text: goal, completed: false, fromPartner }))];
 		await updateGoals(chatId, updatedGoals);
-		if (fromPartner) {
-			await sendMessage(chatId, `Goals added to your partner's list!`);
-		} else {
+		if (!fromPartner) {
 			await sendMessage(chatId, 'Goals added successfully!');
 			await listGoals(chatId);
 		}
@@ -40,5 +38,6 @@ async function addHoney(text, chatId) {
 	const newGoals = goalsText.split(',').map((goal) => `🐝 ${goal.trim()}`);
 	await sendMessage(partner.ChatId, `Your partner added the following honey-do items: ${newGoals.join(', ')}`);
 	await saveGoalsAndList(newGoals, partner.ChatId, true);
+	await sendMessage(chatId, 'Honey-do items added successfully!');
 }
 exports.addHoney = addHoney;
