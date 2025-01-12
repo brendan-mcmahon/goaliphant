a9b9889d6b0d2f9105edca0ff5aac548feb74c9e
brendan.mcmahon.bloomerang <brendanjeffreymcmahon@gmail.com>
2025-01-11 22:59:56 -0500

    some weird logs to see what's happening.

--- bot/handlers/createRewardHandler.js
+++ bot/handlers/createRewardHandler.js
@@ -1,5 +1,7 @@
 const { sendMessage, sendError } = require('../bot.js');
 const { updateReward, setChatState, clearChatState, deleteReward, getReward, insertReward } = require('../common/repository.js');
+const repository = require('../common/repository.js');
+console.log(repository);
 
 const steps = [
 	createReward,
